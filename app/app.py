4de42462518cc9b991a14518e840933575e1364f
vo-steven <20imsteven02@gmail.com>
2024-10-24 00:04:37 -0700

    Add link validation and Case handling
    validate if IS a spotify link --> if IS: if IN DB, search; if ISNT IN DB... to be continued.

--- app/app.py
+++ app/app.py
@@ -1,45 +1,61 @@
 from flask import Flask, render_template, url_for, request, redirect
 from pymongo import MongoClient
-import utils
+import utils as u
 
 app = Flask(__name__)
 client = MongoClient("localhost", 27017)
 db = client.bop_database
 bops = db.bops
-access_token = utils.generate_access_token()
+access_token = u.generate_access_token()
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
+    error = None
     if request.method == "POST":
-        bop_link = request.form["bop_link"].split("/")[-1].split('?')[0]
-        # TO ADD: link verification
-        # if link IS a spotify link, BUT ISN'T in my DB
-        return redirect(url_for("get_bop_recs",bop_id=bop_link))
-    return render_template("index.html")
+        bop_link = request.form["bop_link"]
+        if not u.link_validation(bop_link):
+            # HANDLE EDM GENRE OR NOT?
+            error = "Please enter a valid Spotify track link."
+        else:
+            bop_id = bop_link.split("/")[-1].split('?')[0]
+            return redirect(url_for("get_bop_recs",bop_id=bop_id))
+    
+    return render_template("index.html", error=error, in_db=True)
 
 @app.route("/bop/<bop_id>", methods=["GET","POST"])
 def get_bop_recs(bop_id):
+    error = None
+    in_db = True
     if request.method == "POST":
-        new_bop_link = request.form["bop_link"].split("/")[-1].split('?')[0]
-        # TO ADD: link verification, etc
-        return redirect(url_for("get_bop_recs",bop_id=new_bop_link))
+        bop_link = request.form["bop_link"]
+        if not u.link_validation(bop_link):
+            # HANDLE EDM GENRE OR NOT?
+            error = "Please enter a valid Spotify track link."
+            return render_template("index.html",error=error)
+        else:
+            bop_id = bop_link.split("/")[-1].split('?')[0]
+            return redirect(url_for("get_bop_recs",bop_id=bop_id))
 
     # get bop info and bop_db
     res = bops.find_one({"id":bop_id})
-    all_bops = list(bops.find())
+    bop_info = u.get_bop_info(bop_id, access_token)
+    if res:
+        all_bops = list(bops.find())
 
-    bop_info = {
-        "bop_name": res["name"],
-        "artists": res["artists"]
-    }
+        # get recs
+        ids, scores = u.compare_embeddings(res, all_bops, k=6)
+        recommendations = [{"id": id, "score": f"{score.item() * 100:.4g}%"} 
+                        for id, score in zip(ids, scores)]
+    else:
+        recommendations = []
+        in_db = False
+        # ADD NOT IN DB CASE
+        # 1) ADD TO A PLAYLIST, ONCE REACHES N LENGTH, EMBED PLAYLIST
+        # 2) EMBED SONG ON THE SPOT?
 
-    # get recs
-    ids, scores = utils.compare_embeddings(res, all_bops, k=6)
+        # WE DO WANT ABILITY TO EMBED ENTIRE PLAYLISTS SO WE CAN EXPAND QUICKER
 
-    recommendations = [{"id": id, "score": f"{score.item() * 100:.4g}%"} 
-                       for id, score in zip(ids, scores)]
-
-    return render_template("index.html",bop_info=bop_info, recommendations=recommendations)
+    return render_template("index.html",bop_info=bop_info, recommendations=recommendations, in_db=in_db)
 
 if __name__ == "__main__":
     app.run(debug=True)
